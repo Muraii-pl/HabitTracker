abc58fe62897a09a99fb1da9d0875a47900c4683
Muraii-pl <mateusz.glowania@gmail.com>
2021-10-28 00:03:32 +0200

    migrate and seeders - users

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.SERVER_PORT || 3001
-const db = require('./api/models')
+const db = require('./db/models')
 
 const app = express()
 
 
-db.sequelize.sync().then(()=>{
-    app.listen(port, ()=>{
-        console.log(`Server running on port ${port}`)
-    })
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
