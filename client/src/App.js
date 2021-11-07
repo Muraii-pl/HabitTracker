a58e5727afeb5f95774c829c0a5ac71cf9b205c5
Nekoshy <neko@intmail.pl>
2021-11-07 14:19:32 +0100

    new folders/files for future pages / try navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import Home from './pages/Home/Home'
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './pages/Home/Home';
 import './App.css';
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-        <Home/>
+    <div className="container">
+            {/*<Router>*/}
+            {/*    <Navbar/>*/}
+            {/*    <Route path='/' component={Home}/>*/}
+            {/*    <Route path='/login' component={Login}/>*/}
+            {/*    <Route path='/register' component={Register}/>*/}
+            {/*</Router>*/}
+        {/*<Home/>*/}
+        <Register/>
+        {/*<Login/>*/}
     </div>
   );
 }
